366bfc00cc00a45182c266ad44a7145ca960f427
Derek Kaknes <derek.kaknes@gmail.com>
2016-12-16 01:06:33 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,16 @@
+class Genre
+  attr_accessor :name, :songs
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.genre = self
+  end
+
+  def artists
+    self.songs.collect {|s| s.artist}
+  end
+end
