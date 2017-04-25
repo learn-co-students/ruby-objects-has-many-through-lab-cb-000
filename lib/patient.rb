6faeb3187c9bab8b5160e0fca2d5f28b8f54ba8a
natasha kelly <kellynatashav@yahoo.com>
2017-04-25 04:14:28 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,10 +3,16 @@ class Patient
 
   def initialize(name)
     @name = name
+    @appointments = []
+  end
+
+  def appointments
+    @appointments
   end
 
   def add_appointment(appointment)
-
+    @appointments << appointment
+    appointment.patient = self
   end
 
   def doctors
