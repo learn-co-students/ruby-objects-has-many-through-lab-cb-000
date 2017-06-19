24f938dd7357c04b9b856136e39e0b8040f8f7ab
cptbrkfst <dyl@resonategroup.ca>
2017-06-19 19:52:29 -0400

    completed artist portion

---
+++ lib/genre.rb
@@ -0,0 +1,13 @@
+# Genre class
+class Genre
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def artists
+    @songs.collect(&:artist)
+  end
+end
