705104981ae9ef2bfb04434c986bf7ebd65d1cc6
cptbrkfst <dyl@resonategroup.ca>
2017-06-19 20:17:28 -0400

    completed lab

---
+++ lib/appointment.rb
@@ -0,0 +1,10 @@
+# Appointment class
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+    doctor.appointments << self
+  end
+end
